ee3828b91751c992add7442861ad5db44131cb33
Jainam Desai <jainamvipul.desai2019@vitstudent.ac.in>
2021-11-22 13:11:15 +0000

    chores: finish setting up

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { useHistory } from 'react-router';
+import { useHistory } from 'react-router-dom';
 
 import auth from '../utils/firebase';
 
